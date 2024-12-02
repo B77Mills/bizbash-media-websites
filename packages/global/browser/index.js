7f859d826dff4d3958e66bf18b2dcf232886f6e0
Brian Miller <brian@parameter1.com>
2024-12-02 16:31:01 -0600

    remove local version of content meter component
    Thre are not currently being used

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,6 +1,5 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
-const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 const InquiryForm = () => import(/* webpackChunkName: "global-inquiry-form" */ './inquiry-form.vue');
 const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
 
@@ -10,6 +9,5 @@ export default (Browser) => {
     inquiryArgs: { component: InquiryForm },
   });
 
-  Browser.register('ContentMeterTrack', ContentMeterTrack);
   Browser.register('GlobalImageSlider', ImageSlider);
 };
